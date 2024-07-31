199100f8bbad0cf8c026416bbc702455f9a1f551
GalloJunior <gallojunior@gmail.com>
2024-07-31 12:24:14 -0300

    Estilizado o aplicado e cliques do jogo

---
+++ jogo-da-velha/components/figura.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { TouchableOpacity } from 'react-native'
+import { Entypo } from '@expo/vector-icons'
+
+export default function Figura({ vetor, posicao, clicado }) {
+  const getIcon = () => {
+    if (vetor[posicao] !== 0) {
+      return vetor[posicao] ? "cross" : "circle";
+    }
+    return "pencil";
+  }
+
+  return (
+    <TouchableOpacity onPress={clicado}>
+      <Entypo name={getIcon()} size={90} color="#000" />
+    </TouchableOpacity>
+  )
+}
